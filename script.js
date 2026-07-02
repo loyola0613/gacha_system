ab6b0ea5fce511ff8e6ccf6cccbe577428c07d14
loyola0613 <rokko9685@gmail.com>
2026-07-02 17:14:51 +0900

    Add setting function and Add charactors name

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const app = document.getElementById("app");
 
 showMenu();
 
+//ガチャ一覧画面
 async function showMenu() {
 
     const response = await fetch("data/index.json");
@@ -25,15 +26,47 @@ async function showMenu() {
 
 }
 
+//ガチャ画面
 async function showGacha(gacha) {
 
+    const key = `enabled_${gacha.id}`;
     const response = await fetch(`data/${gacha.id}.json`);
     const characters = await response.json();
+
+    //保存済みデータ取得
+    const saved =
+    JSON.parse(localStorage.getItem(key) || "{}");
+
+    //排出OFFのみ除外
+    const enabledCharacters =
+    characters.filter(character => {
+
+        if (saved[character.name] === false) {
+            return false;
+        }
+
+        return true;
+
+    });
 
     app.innerHTML = `
         <button id="back">← ガチャ一覧</button>
 
+        <div class="title-row">
+
         <h1>${gacha.name}</h1>
+
+        <button id="setting" class="icon-button" title="排出キャラ設定">
+            ⚙
+        </button>
+
+        </div>
+
+        <p class="count">
+            <br>
+            登録：<span id="totalCount"></span>人　　
+            排出：<span id="enabledCount"></span>人
+        </p>
 
         <button id="roll">
             🎲 回す
@@ -44,16 +77,130 @@ async function showGacha(gacha) {
         </div>
     `;
 
+    updateCount(characters, saved);
+
     document.getElementById("back").onclick = showMenu;
 
     document.getElementById("roll").onclick = () => {
 
+        //ダイスロール時に排出数0の場合
+        if(enabledCharacters.length===0){
+            result.textContent="排出キャラがいません";
+            return;
+        }
+
         const random =
-            Math.floor(Math.random() * characters.length);
+            Math.floor(Math.random() * enabledCharacters.length);
 
         document.getElementById("result").textContent =
-            characters[random];
+            characters[random].name;
 
     };
+
+    document.getElementById("setting").onclick=()=>{
+
+        showSettings(
+            gacha,
+            characters
+        );
+
+    };
+
+}
+
+//排出セッティングページ
+function showSettings(
+    gacha,
+    characters
+){
+    const key = `enabled_${gacha.id}`;
+
+    const saved =
+        JSON.parse(
+            localStorage.getItem(key) || "{}"
+        );
+
+    app.innerHTML = `
+        <button id="back">
+            ←戻る
+        </button>
+
+        <h1>${gacha.name}</h1>
+
+        <p class="count">
+            登録：<span id="totalCount"></span>人　　
+            排出：<span id="enabledCount"></span>人
+        </p>
+
+        <div id="list"></div>
+
+        <button id="back">
+            ←戻る
+        </button>
+    `;
+
+    updateCount(characters, saved);
+
+    const list =
+        document.getElementById("list");
+
+    //ループ
+    characters.forEach(character=>{
+        const label =
+            document.createElement("label");
+
+        const checkbox =
+            document.createElement("input");
+
+            checkbox.type="checkbox";
+
+        //保存がなければON
+        checkbox.checked =
+            saved[character.name] !== false;
+
+        //切り替え
+        checkbox.onchange=()=>{
+
+            saved[character.name] = checkbox.checked;
+
+            localStorage.setItem(
+                key,
+                JSON.stringify(saved)
+            );
+
+            updateCount(characters, saved);
+        };
+
+        //表示
+        label.appendChild(checkbox);
+        label.append(
+            " " + character.name
+        );
+        list.appendChild(label);
+        list.appendChild(
+            document.createElement("br")
+        );
+
+        document
+        .getElementById("back")
+        .onclick=()=>{
+
+        showGacha(gacha);
+        };
+    });
+}
+
+//人数を表示する関数
+function updateCount(characters, saved){
+
+    const total = characters.length;
+
+    const enabled =
+        characters.filter(character =>
+            saved[character.name] !== false
+        ).length;
+
+    document.getElementById("totalCount").textContent = total;
+    document.getElementById("enabledCount").textContent = enabled;
 
 }
